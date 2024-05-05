9594c051ef9f9486e31e97daea8263b7c8a4f804
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-05-05 11:09:43 -0300

    Criando o componente message

--- src/app/interfaces/Comment.ts
+++ src/app/interfaces/Comment.ts
@@ -2,7 +2,7 @@ export interface Comment {
   id?: string;
   text: string;
   username: string;
-  momentId: number;
   createdAt?: string;
   updatedAt?: string;
+  momentId: number;
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseApiUrl: 'https://'
+  baseApiUrl: 'https://localhost:7058/'
 };
